77707a9f772cfa5b3c4b85fd8397b286c8363660
Vainius Mykolaitis <vainius.mykolaitis@mygom.tech>
2024-10-09 22:13:21 +0300

    capitalize locale

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
 import { NextResponse, NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-type Countries = "fr" | "lt" | null;
+type Countries = "FR" | "LT" | null;
 
 export function middleware(request: NextRequest) {
   const country = (request.geo && request.geo.country) as Countries;
 
   const mappedRoutes = {
-    fr: "/about/france",
-    lt: "/about/lithuania",
+    FR: "/about/france",
+    LT: "/about/lithuania",
     default: "/about",
   };
 
